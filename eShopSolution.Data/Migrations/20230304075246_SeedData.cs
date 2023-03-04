45297eb1865aea911973330f0eea094b69736e13
Vinamilk <nhatnguyen20102002@gmail.com>
2023-03-04 14:58:00 +0700

    Seeding_DataBase

---
+++ eShopSolution.Data/Migrations/20230304075246_SeedData.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eShopSolution.Data.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CategoryTranslations_Categories_CategoryId",
+                table: "CategoryTranslations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CategoryTranslations_Languages_LanguageId",
+                table: "CategoryTranslations");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CategoryTranslations",
+                table: "CategoryTranslations");
+
+            migrationBuilder.RenameTable(
+                name: "CategoryTranslations",
+                newName: "CategoryTránlations");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CategoryTranslations_LanguageId",
+                table: "CategoryTránlations",
+                newName: "IX_CategoryTránlations_LanguageId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CategoryTranslations_CategoryId",
+                table: "CategoryTránlations",
+                newName: "IX_CategoryTránlations_CategoryId");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "Orders",
+                nullable: false,
+                defaultValue: new DateTime(2023, 3, 4, 14, 52, 45, 281, DateTimeKind.Local).AddTicks(7809),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2023, 3, 2, 1, 48, 7, 275, DateTimeKind.Local).AddTicks(7291));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CategoryTránlations",
+                table: "CategoryTránlations",
+                column: "Id");
+
+            migrationBuilder.InsertData(
+                table: "AppConfigs",
+                columns: new[] { "Key", "Value" },
+                values: new object[,]
+                {
+                    { "HomeTitle", "This is home page of eshopSolution" },
+                    { "HomeKeyword", "This is keyword of eshopSolution" },
+                    { "HomeDescription", "This is description of eshopSolution" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "IsShowOnHome", "ParentId", "SortOrder", "Status" },
+                values: new object[,]
+                {
+                    { 1, true, null, 1, 1 },
+                    { 2, true, null, 2, 1 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Languages",
+                columns: new[] { "Id", "IsDefault", "Name" },
+                values: new object[,]
+                {
+                    { "vi-VN", true, "Tiếng Việt" },
+                    { "en-US", false, "English" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "DateCreated", "OriginalPrice", "Price", "SeoAlias" },
+                values: new object[] { 1, new DateTime(2023, 3, 4, 14, 52, 45, 297, DateTimeKind.Local).AddTicks(656), 100000m, 200000m, null });
+
+            migrationBuilder.InsertData(
+                table: "CategoryTránlations",
+                columns: new[] { "Id", "CategoryId", "LanguageId", "Name", "SeoAlias", "SeoDescription", "SeoTitle" },
+                values: new object[,]
+                {
+                    { 1, 1, "vi-VN", "Áo nam", "ao-nam", "Sản phẩm áo thời trang nam", "Sản phẩm áo thời trang nam" },
+                    { 3, 2, "vi-VN", "Áo nữ", "ao-nu", "Sản phẩm áo thời trang nữ", "Sản phẩm áo thời trang nữ" },
+                    { 2, 1, "en-US", "Man Shirt", "man-shirt", "The shirt products for man", "The shirt products for man" },
+                    { 4, 2, "en-US", "Women Shirt", "women-shirt", "The shirt products for women", "The shirt products for women" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductInCategories",
+                columns: new[] { "CategoryId", "ProductId" },
+                values: new object[] { 1, 1 });
+
+            migrationBuilder.InsertData(
+                table: "ProductTranslations",
+                columns: new[] { "Id", "Description", "Details", "LanguageId", "Name", "ProductId", "SeoAlias", "SeoDescription", "SeoTitle" },
+                values: new object[,]
+                {
+                    { 1, "Áo sơ mi nam trắng Việt Tiến", "Áo sơ mi nam trắng Việt Tiến", "vi-VN", "Áo sơ mi nam trắng Việt Tiến", 1, "ao-so-mi-nam-trang-viet-tien", "Áo sơ mi nam trắng Việt Tiến", "Áo sơ mi nam trắng Việt Tiến" },
+                    { 2, "Viet Tien Man T-Shirt", "Viet Tien Man T-Shirt", "en-US", "Viet Tien Man T-Shirt", 1, "viet-tien-men-t-shirt", "Viet Tien Man T-Shirt", "Viet Tien Man T-Shirt" }
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CategoryTránlations_Categories_CategoryId",
+                table: "CategoryTránlations",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CategoryTránlations_Languages_LanguageId",
+                table: "CategoryTránlations",
+                column: "LanguageId",
+                principalTable: "Languages",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CategoryTránlations_Categories_CategoryId",
+                table: "CategoryTránlations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CategoryTránlations_Languages_LanguageId",
+                table: "CategoryTránlations");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CategoryTránlations",
+                table: "CategoryTránlations");
+
+            migrationBuilder.DeleteData(
+                table: "AppConfigs",
+                keyColumn: "Key",
+                keyValue: "HomeDescription");
+
+            migrationBuilder.DeleteData(
+                table: "AppConfigs",
+                keyColumn: "Key",
+                keyValue: "HomeKeyword");
+
+            migrationBuilder.DeleteData(
+                table: "AppConfigs",
+                keyColumn: "Key",
+                keyValue: "HomeTitle");
+
+            migrationBuilder.DeleteData(
+                table: "CategoryTránlations",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "CategoryTránlations",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "CategoryTránlations",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "CategoryTránlations",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "ProductInCategories",
+                keyColumns: new[] { "CategoryId", "ProductId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "ProductTranslations",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "ProductTranslations",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Languages",
+                keyColumn: "Id",
+                keyValue: "en-US");
+
+            migrationBuilder.DeleteData(
+                table: "Languages",
+                keyColumn: "Id",
+                keyValue: "vi-VN");
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.RenameTable(
+                name: "CategoryTránlations",
+                newName: "CategoryTranslations");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CategoryTránlations_LanguageId",
+                table: "CategoryTranslations",
+                newName: "IX_CategoryTranslations_LanguageId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CategoryTránlations_CategoryId",
+                table: "CategoryTranslations",
+                newName: "IX_CategoryTranslations_CategoryId");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2023, 3, 2, 1, 48, 7, 275, DateTimeKind.Local).AddTicks(7291),
+                oldClrType: typeof(DateTime),
+                oldDefaultValue: new DateTime(2023, 3, 4, 14, 52, 45, 281, DateTimeKind.Local).AddTicks(7809));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CategoryTranslations",
+                table: "CategoryTranslations",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CategoryTranslations_Categories_CategoryId",
+                table: "CategoryTranslations",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CategoryTranslations_Languages_LanguageId",
+                table: "CategoryTranslations",
+                column: "LanguageId",
+                principalTable: "Languages",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
